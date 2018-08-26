74b1c46164e86083360859e16a83efe008005805
Gonzalo Reyes Huertas <gonzalorehu.97.31@gmail.com>
2018-08-25 22:33:16 -0500

    Added feature selection and data encoding methods

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from data.data_manager import DataManager
 
-from settings import FILE_PATH, VARIABLE_TO_CLASSIFY
+from settings import FILE_PATH
+from settings import VARIABLE_TO_CLASSIFY, VARIABLES_TO_DELETE
+from settings import VARIABLES_TO_OH_ENCODE, VARIABLES_TO_B_ENCODE
 from settings import NUMBER_OF_GENERATIONS, TARGET_FITNESS
 
 from helper_methods.print_methods import print_population
@@ -8,7 +10,11 @@ from helper_methods.print_methods import print_population
 def setup_data_manager():
     data_manager = DataManager.shared()
     data_manager.read_data(FILE_PATH)
+    data_manager.drop_columns(VARIABLES_TO_DELETE)
+    data_manager.one_hot_encode_data(VARIABLES_TO_OH_ENCODE)
+    data_manager.binary_encode_data(VARIABLES_TO_B_ENCODE)
     data_manager.split_data(VARIABLE_TO_CLASSIFY)
+    data_manager.rfe_feature_selection(15)
     data_manager.print_variables()
 
 def setup_population():
@@ -25,19 +31,18 @@ def setup_population():
 
 def main():
     setup_data_manager()
-    dm = DataManager.shared()
 
-    from genetic_algorithm.genetic_algorithm import GeneticAlgorithm
+    # from genetic_algorithm.genetic_algorithm import GeneticAlgorithm
 
-    population = setup_population()
-    generation_number = 1
+    # population = setup_population()
+    # generation_number = 1
 
-    while generation_number != NUMBER_OF_GENERATIONS and population.get_chromosomes()[0].get_fitness() < TARGET_FITNESS:
-        population = GeneticAlgorithm.evolve(population)
-        population.calculate_fitness(dm.get_X_train(), dm.get_y_train())
-        population.get_chromosomes().sort(key=lambda ann: ann.get_fitness(), reverse = True)
-        print_population(population, generation_number)
-        generation_number += 1
+    # while generation_number != NUMBER_OF_GENERATIONS and population.get_chromosomes()[0].get_fitness() < TARGET_FITNESS:
+    #     population = GeneticAlgorithm.evolve(population)
+    #     population.calculate_fitness(dm.get_X_train(), dm.get_y_train())
+    #     population.get_chromosomes().sort(key=lambda ann: ann.get_fitness(), reverse = True)
+    #     print_population(population, generation_number)
+    #     generation_number += 1
 
 if __name__ == '__main__':
     main()
